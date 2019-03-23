599a49f624d0b3238495b1d2e544897addfe5517
iwanfrancis <iwanfrancis@gmail.com>
2019-03-23 19:21:12 +0000

    Score persists between scenes

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -11,7 +11,17 @@ public class GameStatus : MonoBehaviour {
     [SerializeField] Text scoreText;
 
     // State variables
-    [SerializeField] int currentScore = 0;
+    int currentScore = 0;
+
+    private void Awake() {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1) {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     private void Start() {
         scoreText.text = currentScore.ToString();
